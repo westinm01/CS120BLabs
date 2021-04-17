1461cb00d5ec6531662f23abc2746f8ddf77bf7d
Westin Montano <wmont012@wch136-24.cs.ucr.edu>
2021-04-16 22:19:57 -0700

    Completed part 3

---
+++ Lab4_stateMachines/turnin/wmont012_lab4_part3.c
@@ -0,0 +1,108 @@
+/*	Author: wmont012
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+enum STATES {init, poundHold, poundRelease, unlocked} state;
+
+unsigned char pa0;	//X
+unsigned char pa1;	//Y
+unsigned char pa2;	//pound
+unsigned char pa7;	//lock
+
+void TickFct(){
+	switch(state){
+		case init:
+			if(pa2 && !pa0 &&!pa1 &&!pa7){
+				state=poundHold;
+			}
+			else{
+				state=init;
+			}
+		break;
+		case poundHold:
+			if(pa2 && !pa0 && !pa1 && !pa7){
+                                state=poundHold;
+                        }
+			else if(!pa2 && !pa0 && !pa1 && !pa7){
+                                state=poundRelease;
+                        }
+			else if(!pa2 && !pa0 && pa1 &&!pa7){
+				state=unlocked;		
+			}	
+			else{
+				state=init;
+			}
+		break;	
+		case poundRelease:
+			if(pa1 && !pa0 && !pa2 &&!pa7){
+				state=unlocked;
+			}
+			else if (!pa1 && !pa0 && !pa2 && !pa7){
+				state=poundRelease;
+			}
+			else{
+				state=init;
+			}
+		break;
+		case unlocked:
+			if (!pa7){
+                                state=unlocked;
+                        }
+			else{
+				state=init;
+			}
+		break;
+		default:
+			state=init;
+		break;
+	}
+	switch(state){
+		case init:
+			PORTC=0x00;
+			PORTB=0x00;
+		break;
+		case poundHold:
+			PORTC=0x01;
+			PORTB=0x00;
+		break;
+		case poundRelease:
+			PORTC=0x02;
+			PORTB=0x00;
+		break;
+		case unlocked:
+			PORTC=0x03;
+			PORTB=0x01;
+			
+		break;
+		default:
+		break;
+	}
+}
+
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRA=0x00; PORTA=0xFF;
+	DDRB=0xFF; PORTB=0x00;
+	DDRC=0xFF; PORTC=0x00;	
+	state= init;
+    /* Insert your solution below */
+    while (1) {
+	pa0=PINA & 0x01;
+	pa1=(PINA>>1)&0x01;
+	pa2=(PINA>>2)&0x01;
+	pa7=(PINA>>7)&0x01;
+	TickFct();
+    }
+    return 1;
+}
